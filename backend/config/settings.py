16812b1ea52611de661123afb78f9b14602642c6
Juan Carlos <ptonjk03@protonmail.com>
2026-03-18 10:27:04 +0100

    Estructura de carpetas del proyecto

--- backend/config/settings.py
+++
@@ -1,1 +0,0 @@
-"""Ajustes generales, bases de datos, seguridad"""
--- backend/config/urls.py
+++
@@ -1,9 +0,0 @@
-"""Rutas principales (APIs, admin, etc)"""
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", include("api.urls")),
-]
